e2f847a036a1159dda1959f7cef7c69714fe69d8
willian brandão mendonça <willianbr.ac68@gmail.com>
2021-11-12 13:57:03 -0500

    success

---
+++ src/modules/users/repositories/implementations/UsersRepository.ts
@@ -0,0 +1,39 @@
+import { getRepository, Repository } from "typeorm";
+
+import { IFindUserWithGamesDTO, IFindUserByFullNameDTO } from "../../dtos";
+import { User } from "../../entities/User";
+import { IUsersRepository } from "../IUsersRepository";
+
+export class UsersRepository implements IUsersRepository {
+  private repository: Repository<User>;
+
+  constructor() {
+    this.repository = getRepository(User);
+  }
+
+  async findUserWithGamesById({
+    user_id,
+  }: IFindUserWithGamesDTO): Promise<User> {
+    return await this.repository.findOneOrFail(
+      { id: user_id },
+      { relations: ["games"] }
+    );
+  }
+
+  async findAllUsersOrderedByFirstName(): Promise<User[]> {
+    return this.repository.query(`
+    SELECT *
+    FROM users
+    ORDER BY first_name ASC
+  `);
+  }
+
+  async findUserByFullName({
+    first_name,
+    last_name,
+  }: IFindUserByFullNameDTO): Promise<User[] | undefined> {
+    return this.repository.query(
+      `SELECT * FROM users WHERE LOWER(first_name)=LOWER('${first_name}') AND LOWER(last_name)=LOWER('${last_name}') LIMIT 1`
+    );
+  }
+}
